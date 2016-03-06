acc64db4d27067fe1769a1413921eb169c7987e5
kooh-z <kody.j.gigs@gmail.com>
2016-03-07 00:51:42 +0900

    Enable to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -55,7 +56,11 @@ class User < ActiveRecord::Base
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-  
+
+  def feed
+    Post.where("user_id = ?", id)
+  end
+
   private
 
     def downcase_email

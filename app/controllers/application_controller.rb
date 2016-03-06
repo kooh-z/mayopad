fa93a7158d696108e58c18b5f0434d2fe449d51d
kooh-z <kody.j.gigs@gmail.com>
2016-03-07 02:49:57 +0900

    Add other users feeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   protect_from_forgery with: :exception
   include SessionsHelper
 
   private
-  
+
     def logged_in_user
       unless logged_in?
         store_location

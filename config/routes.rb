636f90f90dddc822a6ae2f540a1a6201d6270c0a
kooh-z <kody.j.gigs@gmail.com>
2016-03-12 18:05:08 +0900

    Modify About page

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   get 'searches/index'
 
   root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
